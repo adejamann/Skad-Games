0710e46eeb7ddb86be09ad04b914afbeb3f93c2f
adejamann <106279403+adejamann@users.noreply.github.com>
2022-06-05 20:00:15 -0400

    Add files via upload

--- GameSuite.java
+++ GameSuite.java
@@ -18,7 +18,7 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.layout.GridPane;
 
 /**
- *@author Team 17
+ *@author Adeja Mann
  *@version 1.0 
  */
 public class GameSuite extends Application {
@@ -35,12 +35,11 @@ public class GameSuite extends Application {
      */
     public void start(Stage primaryStage) {
     //Title
-        primaryStage.setTitle("Skad Games");
-        Text label = new Text("SKAD Games");
-        label.setFont(Font.font("Verdana", FontWeight.EXTRA_BOLD, 65));
-        label.setUnderline(true);
+        primaryStage.setTitle("Game Suite");
+        Text label = new Text("SKAD GAMES");
+        label.setFont(Font.font("Comic Sans MS", FontWeight.EXTRA_BOLD, 65));
         label.setFill(Color.WHITE);
-        label.setStrokeWidth(2);
+        label.setStrokeWidth(5);
         label.setStroke(Color.BLACK);
         HBox title = new HBox(label);
         title.setAlignment(Pos.TOP_CENTER);
@@ -52,15 +51,29 @@ public class GameSuite extends Application {
         sq2.setFill(Color.WHITE);
         Rectangle sq3 = new Rectangle(100, 100, 100, 100);
         sq3.setFill(Color.WHITE);
+        StackPane sq1stack = new StackPane();
+        Text sq1label = new Text("BlackJack");
+        sq1label.setFont(Font.font("Comic Sans MS", FontWeight.EXTRA_BOLD, 20));
+        sq1stack.getChildren().addAll(sq1, sq1label);
+        StackPane sq2stack = new StackPane();
+        Text sq2label = new Text("Wordle");
+        sq2label.setFont(Font.font("Comic Sans MS", FontWeight.EXTRA_BOLD, 20));
+        sq2stack.getChildren().addAll(sq2, sq2label);
+        StackPane sq3stack = new StackPane();
+        Text sq3label = new Text("Tic-Tac-Toe");
+        sq3label.setFont(Font.font("Comic Sans MS", FontWeight.EXTRA_BOLD, 15));
+        sq3stack.getChildren().addAll(sq3, sq3label);
         GridPane grid = new GridPane();
-        grid.add(sq1, 0, 0, 1, 1);
-        grid.add(sq2, 1, 0, 1, 1);
-        grid.add(sq3, 2, 0, 1, 1);
+        grid.add(sq1stack, 0, 0, 1, 1);
+        grid.add(sq2stack, 1, 0, 1, 1);
+        grid.add(sq3stack, 2, 0, 1, 1);
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(15);
+        
+        
 
         // Background
-        Image img = new Image("background2.JPG");
+        Image img = new Image("images.JPG");
         ImageView bg = new ImageView(img);
         bg.setFitHeight(500);
         bg.setFitWidth(500);
