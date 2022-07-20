846c3e35335cc95a1b494a730f22856a801270a7
cejikeme3 <cejikeme3@gatech.edu>
2022-07-20 18:26:46 -0400

    checking funtionality of addHand method

--- src/Dealer.java
+++ src/Dealer.java
@@ -12,29 +12,6 @@ public class Dealer {
       hand.add(c);
    }
    
-   /*
-   * takes an arraylist of cards and adds it back to the deck 
-   */
-   public void addDeck(ArrayList<Card> a) {
-      for (int i = 0; i < a.size(); i++) {
-         Card c = a.remove(i);
-         deck.add(c);
-      }
-   }
-   
-   // takes in the card to remove and returns the removed card
-   // the card is no longer in the hand of the dealer
-   public Card removeHand(Card c) {
-      Card c = hand.remove(hand.indexOf(c));
-      return c;
-   }
-   
-   // removes from the deck
-   // the card should no longer be in the deck anymore
-   public Card removeDeck(Card c) {
-      Card c = deck.remove(deck.indexOf(c)); 
-      return c; 
-   }
-   
+      
    
 }
